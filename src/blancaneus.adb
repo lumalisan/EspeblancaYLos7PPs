380c3a34b434f2b9abd6b64aa377f2de6bdf86ed
PussyDestroyer69 <lumalisan@gmail.com>
2019-11-22 10:51:40 +0100

    Proyecto pasado a OwAdA

---
+++ src/blancaneus.adb
@@ -0,0 +1,73 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+with def_monitor;
+use def_monitor;
+with Ada.Strings.Unbounded;
+
+procedure BlancaNeus is
+
+   MAX_NANS : constant Integer := 7;
+
+   mon : Monitor;
+
+   -- ** Especificacio **
+   -- Blanca Neus
+   task type Blanca is
+      entry Start (Idx: in Integer);
+   end Blanca;
+
+   -- Nan
+   task type Nan is
+      entry Start (Idx: in Integer);
+   end Nan;
+
+   -- ** Cos **
+   -- Blanca Neus
+   task body Blanca is
+      My_Idx: Integer;
+   begin
+      accept Start (Idx : in Integer) do
+         My_Idx := Idx;
+      end Start;
+
+      Put_Line("Blancaneus se'n va a fer una passejada");
+
+
+   end Blanca;
+
+   -- Nan
+   task body Nan is
+      My_Idx : Integer;
+      -- max : Integer := 10;
+   begin
+      accept Start (Idx : in Integer) do
+         My_Idx := Idx;
+      end Start;
+
+      Put_Line (noms'Img(My_Idx) & " treballa a la mina");
+
+
+   end Nan;
+
+   -- Variables
+   type a_nans is array (1..MAX_NANS) of Nan;
+   type a_noms is array (1..MAX_NANS) of Ada.Strings.Unbounded.Unbounded_String;
+   noms : a_noms;
+   n : a_nans;
+   b: Blanca;
+
+begin
+   noms(1) := Ada.Strings.Unbounded.To_Unbounded_String("Mudet");
+   noms(2) := Ada.Strings.Unbounded.To_Unbounded_String("Esternuts");
+   noms(3) := Ada.Strings.Unbounded.To_Unbounded_String("Dormilega");
+   noms(4) := Ada.Strings.Unbounded.To_Unbounded_String("Vergonyos");
+   noms(5) := Ada.Strings.Unbounded.To_Unbounded_String("Felis");
+   noms(6) := Ada.Strings.Unbounded.To_Unbounded_String("Rondinaire");
+   noms(7) := Ada.Strings.Unbounded.To_Unbounded_String("Savi");
+   b.Start(0);
+   Put_Line ("BON DIA som na Blancaneus");
+   for Idx in 1..MAX_NANS loop
+      n(Idx).Start(Idx);
+      Put_Line ("BON DIA som en " & noms'Img(Idx));
+   end loop;
+end BlancaNeus;
---
+++ src/def_monitor.adb
@@ -0,0 +1,21 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+package body def_monitor is
+
+   protected body Monitor is
+      entry menjar when (assegut = true and servit = true) is
+      begin
+         Put_Line("--------------> {Nombre Enano} Menjant!!!");
+         assegut := false;
+         servit := false;
+      end menjar;
+
+      entry put_honey when (jar < MAX) is
+      begin
+         jar := jar + 1;
+         Put_Line("jar "&jar'Img&"/"&MAX'Img);
+      end put_honey;
+   end Monitor;
+
+end def_monitor;
